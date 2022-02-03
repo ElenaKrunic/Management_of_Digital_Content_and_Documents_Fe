579ac4ff2e2659855d484d8bf0c41459a56b5140
Elena Krunic <elenakrunic@gmail.com>
2022-02-03 10:51:46 +0100

    user can leave comment&grade

--- front/src/views/errand/ErrandDetails.vue
+++ front/src/views/errand/ErrandDetails.vue
@@ -1,24 +1,32 @@
 <template>
-<div class="container">
-  <div class="col-md-12">
-    <div class="card">
-
-        <h1> Details about delivery </h1>
-
-      <div class="caption-full">
-        <p> {{errand.orderedAtDate}} </p>
-        <p> {{errand.delivered}} </p>
-        <p> {{errand.grade }} </p>
-        <p> {{errand.comment }} </p>
-        <p> {{errand.anonymousComment }} </p>
-        <p> {{errand.archivedComment }} </p>
-      </div>
-
+<div style="width: 50%; background:white; padding: 5%; border-radius:2%;"> 
+    <h1> Details about delivery </h1>
+    <div class="mb-3">
+        <label class="form-label" for="path">Ordered at date:</label>                
+        <input readonly class="form-control" id="path" v-model="errand.orderedAtDate" />
+    </div>
+     <div class="mb-3">
+        <label class="form-label" for="path">Grade:</label>                
+        <input  class="form-control" id="path" v-model="errand.grade" />
+    </div>
+     <div class="mb-3">
+        <label class="form-label" for="path">Comment:</label>                
+        <input  class="form-control" id="path" v-model="errand.comment" />
     </div>
 
-          <input type="button" value="Back" onclick="window.history.back()"  class="btn btn-primary" /> 
 
-  </div>
+<p bold class="mb-3"> postaviti checkboxove za ova tri polja!!</p>
+
+    <div class="caption-full">
+    <p> {{errand.delivered}} </p>
+    <p> {{errand.anonymousComment }} </p>
+    <p> {{errand.archivedComment }} </p>
+    </div>
+
+    <button class="btn btn-primary" @click="putCommentAndGrade"> Save changes </button>
+    &nbsp;
+    <input type="button" value="Back" onclick="window.history.back()"  class="btn btn-primary" /> 
+
 </div>
 </template>
 
@@ -31,7 +39,10 @@ export default {
     },
     data(){
         return {
-            errand: {},
+            errand: {
+                comment: "",
+                grade:0
+            },
             errorMsg: '',
             loaderColor: "#5cb85c",
             loaderSize: "50px",
@@ -51,7 +62,18 @@ export default {
                 console.log(error)
                 this.errorMsg = 'Error retriving data'
             })
+        },
+        putCommentAndGrade(){
+            //http://localhost:8085/api/articles/orderArticle/?id=1&quantity=20
+            axios.put('api/errands/commentAndGrade/?id=' + this.id + "&comment=" + this.errand.comment+"&grade=" + this.errand.grade)
+            .then((response) => {
+                console.log(response)
+                this.$router.push('/myErrands');
+            }).catch((error) => {
+                console.log(error)
+            })
         }
+
     }
 }
 </script>
